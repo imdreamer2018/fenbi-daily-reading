eea5a38d33f34ac7144339fc222d05de6b4ff112
yangqian <qian.yang2@thoughtworks.com>
2024-04-25 10:37:15 +0800

    modify

---
+++ script/tree.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
+function listFiles(dir, fileList = []) {
+    const files = fs.readdirSync(dir);
+    files.forEach(function(file) {
+        if (fs.statSync(path.join(dir, file)).isDirectory()) {
+            fileList = listFiles(path.join(dir, file), fileList);
+        } else {
+            fileList.push(path.join(dir, file));
+        }
+    });
+    return fileList;
+}
+
+const fileList = listFiles('../docs');
+fs.writeFileSync('fileList.json', JSON.stringify(fileList, null, 2));
